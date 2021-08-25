49492c7b09d4a63c79eff2215b3d97b51c6f9af5
Alberto Davila <alberto.davila.gomez@gmail.com>
2021-08-25 13:16:14 +0200

    added list of heroes

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Pepito' },
+  { id: 12, name: 'Manolo' },
+  { id: 13, name: 'Marta' },
+  { id: 14, name: 'Beto' },
+  { id: 15, name: 'Romualdo' },
+  { id: 16, name: 'Rosita' },
+  { id: 17, name: 'Carlitos' },
+  { id: 18, name: 'Toñin' },
+  { id: 19, name: 'Charito' },
+  { id: 20, name: 'Joselillo' }
+];
